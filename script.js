a1433b2d083028708832ecae93a6ef727f06b534
Pawel Wojciechowski <pawelw@gmx.com>
2023-07-22 12:27:03 +0200

    glass effect added for pvc menu

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 //create module active
 const active = (() => {
-    // grab pvp and pvc class
+    // grab pvp and pvc class and .glass class
     const pvp = document.querySelector('.pvp');
     const pvc = document.querySelector('.pvc');
+    const glass = document.querySelector('.glass');
+
 
     // on pvc click add active class to pvc and pvp otherwise on pvp click remove active class from pvc and pvp
     pvc.addEventListener('click', () => {
         pvc.classList.add('active');
         pvp.classList.add('active');
+        glass.classList.add('active');
     });
 
     pvp.addEventListener('click', () => {
         pvp.classList.remove('active');
         pvc.classList.remove('active');
+        glass.classList.remove('active');
     });
 })();
 
