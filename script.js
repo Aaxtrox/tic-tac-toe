48e2850e6ef8c1f37d5731380500d6f0249b1c71
Pawel Wojciechowski <pawelw@gmx.com>
2023-07-22 12:43:30 +0200

    glass effect added for both menus

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ const active = (() => {
     // grab pvp and pvc class and .glass class
     const pvp = document.querySelector('.pvp');
     const pvc = document.querySelector('.pvc');
-    const glass = document.querySelector('.glass');
+    const pvp_glass = document.querySelector('.pvp_glass');
+    const pvc_glass = document.querySelector('.pvc_glass');
 
 
     // on pvc click add active class to pvc and pvp otherwise on pvp click remove active class from pvc and pvp
     pvc.addEventListener('click', () => {
         pvc.classList.add('active');
         pvp.classList.add('active');
-        glass.classList.add('active');
+        pvp_glass.classList.remove('active');
+        pvc_glass.classList.add('active');
     });
 
     pvp.addEventListener('click', () => {
         pvp.classList.remove('active');
         pvc.classList.remove('active');
-        glass.classList.remove('active');
+        pvp_glass.classList.add('active');
+        pvc_glass.classList.remove('active');
     });
 })();
 
