dd0c16849a029bd74a1a7a8043288deb3c85da49
Aaxtrox <pawelw@gmx.com>
2023-08-03 20:04:32 +0100

    overall code improvement

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-//create module active to toggle between menus
-const active = (() => {
+// Module for toggling between menus
+const menuToggle = (() => {
+    // DOM elements
     const pvp = document.querySelector('.pvp');
     const pvc = document.querySelector('.pvc');
-    const pvp_glass = document.querySelector('.pvp_glass');
-    const pvc_glass = document.querySelector('.pvc_glass');
-    const pvp_menu = document.querySelector('.pvp_menu');
-    const pvc_menu = document.querySelector('.pvc_menu');
+    const player = document.querySelector('.player');
+    const computer = document.querySelector('.computer');
+    const menuPvp = document.querySelector('.menu-pvp');
+    const menuPvc = document.querySelector('.menu-pvc');
 
-
-    // on pvc click add active class
+    // Toggle functionality on clicking pvc and pvp
     pvc.addEventListener('click', () => {
+        // Add 'active' class to elements
         pvc.classList.add('active');
         pvp.classList.add('active');
-        pvp_glass.classList.add('active');
-        pvc_glass.classList.add('active');
-        pvp_menu.classList.add('active');
-        pvc_menu.classList.add('active');
+        player.classList.add('active');
+        computer.classList.add('active');
+        menuPvp.classList.add('active');
+        menuPvc.classList.add('active');
     });
 
     pvp.addEventListener('click', () => {
+        // Remove 'active' class from elements
         pvp.classList.remove('active');
         pvc.classList.remove('active');
-        pvp_glass.classList.remove('active');
-        pvc_glass.classList.remove('active');
-        pvp_menu.classList.remove('active');
-        pvc_menu.classList.remove('active');
+        player.classList.remove('active');
+        computer.classList.remove('active');
+        menuPvp.classList.remove('active');
+        menuPvc.classList.remove('active');
     });
 })();
 
-// module to add current year to footer
-const year = (() => {
+// Module to add current year to footer
+const addCurrentYear = (() => {
     document.getElementById("year").innerHTML = new Date().getFullYear();
 })();
 
-// module to add preloader
-const loading = (() => {
-    // grab id
+// Module to handle preloader
+const preloader = (() => {
+    // Grab preloader and spinner elements
     const preloader = document.getElementById('preloader');
     const spinner = document.getElementById('spinner');
 
-    // when body loads add loader class to preloader and spinner
-    window.addEventListener('load', loader);
-
-    function loader() {
+    // Add 'loader' class to preloader and spinner after window load
+    window.addEventListener('load', () => {
         preloader.classList.add('loader');
         spinner.classList.add('loader');
-    }
+    });
 })();
 
-// module for toggle button
-const toggle = (() => {
-    // grab id
+// Module for toggle button
+const toggleButton = (() => {
+    // DOM elements
     const btn = document.getElementById('btn');
     const btn1 = document.getElementById('btn1');
     const leftClick = document.getElementById('leftClick');
@@ -58,7 +58,7 @@ const toggle = (() => {
     const leftClick1 = document.getElementById('leftClick1');
     const rightClick1 = document.getElementById('rightClick1');
 
-    // add event listener to button
+    // Add click event listeners to buttons
     leftClick.addEventListener('click', () => {
         btn.style.left = '0';
     });
@@ -76,77 +76,71 @@ const toggle = (() => {
     });
 })();
 
-// module query
-const query = (() => {
-    // if logo_img height is bigger than nav height height
-    const logo_img = document.querySelector('.logo_img');
+// Module to handle query
+const handleQuery = (() => {
+    // DOM elements
+    const logoImg = document.querySelector('.logo-img');
     const nav = document.querySelector('nav');
     const footer = document.querySelector('footer');
 
-    // run every time window is resized
+    // Run on window resize
     window.addEventListener('resize', () => {
-        if (logo_img.clientHeight > nav.clientHeight) {
+        if (logoImg.clientHeight > nav.clientHeight) {
+            // Update nav and footer heights based on logo_img height
             nav.style.height = 'fit-content';
-            // save nav height to variable
             const navHeight = nav.clientHeight;
-            // add nav height to footer
             footer.style.height = `${navHeight}px`;
         }
     });
-    // if windows been loaded
+
+    // Run on window load
     window.addEventListener('load', () => {
-        if (logo_img.clientHeight > nav.clientHeight) {
+        if (logoImg.clientHeight > nav.clientHeight) {
+            // Update nav and footer heights based on logo_img height
             nav.style.height = 'fit-content';
-            // save nav height to variable
             const navHeight = nav.clientHeight;
-            // add nav height to footer
             footer.style.height = `${navHeight}px`;
         }
     });
 })();
 
-// module to resize both menus
-const resize = (() => {
-    // grab id pvp_menu and pvc_menu.active
-    const pvp_menu = document.querySelector('.pvp_menu');
-    const pvc_menu = document.querySelector('.pvc_menu');
-    const first = document.getElementById('first');
-    const mid = document.getElementById('mid');
+// Module to resize both menus
+const resizeMenus = (() => {
+    // DOM elements
+    const menu_pvp = document.querySelector('.menu-pvp');
+    const menu_pvc = document.querySelector('.menu-pvc');
+    const card = document.getElementById('card');
+    const vs_divider = document.querySelector('.vd-divider');
 
-    // run every time window is resized
+    // Run on window resize
     window.addEventListener('resize', () => {
-        // if first height is bigger than pvp_menu height or pvc_menu height
-        if (first.clientHeight > pvp_menu.clientHeight || first.clientHeight > pvc_menu.clientHeight) {
-            // save first height to variable and add 10px
-            const firstHeight = first.clientHeight + 5;
-            // add first height to pvp_menu and pvc_menu
-            pvp_menu.style.height = `${firstHeight}px`;
-            pvc_menu.style.height = `${firstHeight}px`;
-            // change top of pvp_menu and pvc_menu to place them in the middle
-            pvp_menu.style.top = `calc(50% - ${firstHeight / 2}px)`;
-            pvc_menu.style.top = `calc(50% - ${firstHeight / 2}px)`;
+        if (card.clientHeight > menu_pvp.clientHeight || card.clientHeight > menu_pvc.clientHeight) {
+            // Calculate new heights and update menu positions
+            const cardHeight = card.clientHeight + 5;
+            menu_pvp.style.height = `${cardHeight}px`;
+            menu_pvp.style.top = `calc(50% - ${cardHeight / 2}px)`;
+            menu_pvc.style.height = `${cardHeight}px`;
+            menu_pvc.style.top = `calc(50% - ${cardHeight / 2}px)`;
         }
     });
 
-    // if windows been loaded
+    // Run on window load
     window.addEventListener('load', () => {
-        // if first height is bigger than pvp_menu height or pvc_menu height
-        if (first.clientHeight > pvp_menu.clientHeight || first.clientHeight > pvc_menu.clientHeight) {
-            // save first height to variable and add 10px
-            const firstHeight = first.clientHeight + 5;
-            // add first height to pvp_menu and pvc_menu
-            pvp_menu.style.height = `${firstHeight}px`;
-            pvc_menu.style.height = `${firstHeight}px`;
-            // change top of pvp_menu and pvc_menu to place them in the middle
-            pvp_menu.style.top = `calc(50% - ${firstHeight / 2}px)`;
-            pvc_menu.style.top = `calc(50% - ${firstHeight / 2}px)`;
+        if (card.clientHeight > menu_pvp.clientHeight || card.clientHeight > menu_pvc.clientHeight) {
+            // Calculate new heights and update menu positions
+            const cardHeight = card.clientHeight + 5;
+            menu_pvp.style.height = `${cardHeight}px`;
+            menu_pvp.style.top = `calc(50% - ${cardHeight / 2}px)`;
+            menu_pvc.style.height = `${cardHeight}px`;
+            menu_pvc.style.top = `calc(50% - ${cardHeight / 2}px)`;
         }
     });
 })();
-// run module
-active;
-year;
-loading;
-toggle;
-query;
-resize;
+
+// Run all modules
+menuToggle;
+addCurrentYear;
+preloader;
+toggleButton;
+handleQuery;
+resizeMenus;
